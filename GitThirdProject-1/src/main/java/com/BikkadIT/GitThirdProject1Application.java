f4313ec926121b18a279104f39f7800452d00dd5
sumitchahare <chaharesumit@gmail.com>
2022-08-16 16:43:46 +0530

    project added in central repository

---
+++ GitThirdProject-1/src/main/java/com/BikkadIT/GitThirdProject1Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitThirdProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitThirdProject1Application.class, args);
+	}
+
+}
